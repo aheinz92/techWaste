55ad11c0e328196c51e47700e4b5ad7d4ddc4e30
aheinz92 <aheinz92@uw.edu>
2024-02-01 05:48:31 -0800

    Changed Flask app file name and updated Procfile, and added second line to Procfile. Praying this helps

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/education')
 def education():
     return render_template('education.html')
 
+
 @app.route('/resources')
 def resources():
     return render_template('resources.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
