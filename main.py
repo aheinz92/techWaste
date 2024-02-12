eab8fadacb3563647f442ad4cf820c4046b5c1d6
aheinz92 <aheinz92@uw.edu>
2024-02-12 12:51:54 -0800

    Added lesson pages and much more fake preview content

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ def education():
 def resources():
     return render_template('resources.html')
 
+@app.route('/lesson1')
+def lesson1():
+    return render_template('lesson1.html')
+
+@app.route('/lesson2')
+def lesson2():
+    return render_template('lesson2.html')
+
+@app.route('/lesson3')
+def lesson3():
+    return render_template('lesson3.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
